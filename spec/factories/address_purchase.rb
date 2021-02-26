b042b0115c4c318bb8baefcec45d3ad7f21a87a3
張文博 <choufumihiro@choufumihironoMacBook-Pro.local>
2021-02-26 17:15:15 +0900

    単体テストコード

---
+++ spec/factories/address_purchase.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :address_purchase do
+    post_number { '123-4567' }
+    prefecture_id { 3 }
+    city { '東京都' }
+    street { '世田谷区' }
+    building { '' }
+    phone { '08048484987' }
+  end
+end
