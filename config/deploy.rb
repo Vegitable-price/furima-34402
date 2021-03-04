afbf65acdcd93d267012f709772396adc7bd7cf6
張文博 <choufumihiro@choufumihironoMacBook-Pro.local>
2021-03-04 12:00:06 +0900

    j

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock ( 3.1)
 
 # Capistranoのログの表示に利用する
-set :application, 'furima_34402'
+set :application, 'furima-34402'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:Vegitable-price/furima-34402.git'
